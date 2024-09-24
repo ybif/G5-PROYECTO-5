150d9aa21ad7b10705e6bea63852c2c8d0014c0d
ybif <jiperez16@uan.edu.co>
2024-09-24 10:15:15 -0500

    ejemplo

---
+++ ejemplo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout<<"Bienvenido a mi app para gestionar la edad de las personas. "<<endl;
+    int edades[4];
+    int i; // variable indice
+    
+    for (i=0 ; i<4 ; i++ )
+    {
+        cin>>edades[i];
+    }    
+}
